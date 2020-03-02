a462a0850f4a1e12dd853df01c253f525989f067
Luuk99 <luuk9926@gmail.com>
2020-03-02 20:37:43 +0100

    Baseline App - added a baseline app
    A new baseline app has been added as well as changes to other apps

---
+++ Apps/Baseline/README.md
@@ -0,0 +1,5 @@
+# Baseline App
+This app is used to acquire a baseline energy consumption from the phone.
+
+## How does it work?
+This app does nothing and like the other apps has no UI at all, therefore it should consume no power and therefore the base power consumption of the phone can be measured.
--- Apps/CameraTest/app/src/main/java/e/www/cameratest/MainActivity.kt
+++ Apps/CameraTest/app/src/main/java/e/www/cameratest/MainActivity.kt
@@ -1,17 +1,20 @@
 package e.www.cameratest
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.ImageFormat
+import android.hardware.camera2.*
+import android.hardware.camera2.params.StreamConfigurationMap
+import android.media.ImageReader
 import android.os.Bundle
-import android.provider.MediaStore
+import android.util.Size
+import android.view.Surface
+import androidx.appcompat.app.AppCompatActivity
 
-// Constants for the frequencies
-val low_frequency = 1000
-val medium_frequency = 2000
-val high_frequency = 3000
 
-// Image capture request argument
-var REQUEST_IMAGE_CAPTURE = 1
+// Variables for camera device and session
+private lateinit var imageReader: ImageReader
+private var cameraId: String? = null
+private lateinit var cameraDevice: CameraDevice
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,20 +22,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // For loop which creates the photos
-        while (REQUEST_IMAGE_CAPTURE <= low_frequency) {
-            dispatchTakePictureIntent()
+        // Create an image reader for the capture session
+        val imageReader = ImageReader.newInstance(ImageFormat.PRIVATE, 1, 1, 1)
+        val imReaderSurface = imageReader.surface
+        val targets: MutableList<Surface> = arrayOf(imReaderSurface).toMutableList()
 
-            REQUEST_IMAGE_CAPTURE += 1
-        }
+        // Set up the camera
+        setupCamera()
+
+        // Create the camera capture session
+        cameraDevice.createCaptureSession(targets, object: CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) {
+                // Do something with `session`
+            }
+            override fun onConfigureFailed(session: CameraCaptureSession) = Unit
+        }, null)
     }
 
-    // Takes a picture
-    private fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    // Sets up the camera
+    private fun setupCamera() {
+        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        try {
+            for (thisCameraId in manager.cameraIdList) {
+                val characteristics =
+                    manager.getCameraCharacteristics(thisCameraId)
+                if (characteristics.get(CameraCharacteristics.LENS_FACING) != CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue
+                }
+                cameraId = thisCameraId
+                val streamConfigurationMap: StreamConfigurationMap =
+                    characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+                val sizes: Array<Size> =
+                    streamConfigurationMap.getOutputSizes(ImageFormat.RAW_SENSOR)
+                val picWidth = sizes[0].width
+                val picHeight = sizes[0].height
+                imageReader =
+                    ImageReader.newInstance(picWidth, picHeight, ImageFormat.PRIVATE, 1)
+                imageReader.setOnImageAvailableListener(onImageAvailableListener, backgroundHandler)
             }
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        } catch (e: NullPointerException) {
+            e.printStackTrace()
         }
     }
 }
