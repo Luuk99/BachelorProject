c770e2bafe065148b3f8f856a2c4ee0c816941ad
Luuk99 <luuk9926@gmail.com>
2020-04-09 22:30:07 +0200

    HTTPS Request Test - Finished app

---
+++ Apps/HttpsRequestTest/README.md
@@ -0,0 +1,16 @@
+# HTTPS Request Test
+This app tests the networking energy consumption by sending HTTPS GET requests.
+
+## How does it work?
+This app engages a loop which sends a HTTPS GET request to Google on an interval determined by the frequency provided. 
+
+## Frequencies
+For this app we used the following frequencies:
+* Low frequency: 3 seconds
+* Medium frequency: 2 seconds
+* High frequency: 1 second
+
+## Changing the frequency
+You can easily change the frequency by opening the project in Android Studio, go to the MainActivity.kt class look for the sentence 
+``` handler.postDelayed(runnable, low_frequency) ```.
+Here you can change the frequency by simply substituting ```low_frequency``` with ```medium_frequency``` or ```high_frequency```.
---
+++ Apps/HttpsRequestTest/app/src/main/java/e/www/httpsrequesttest/MainActivity.kt
@@ -0,0 +1,61 @@
+package e.www.httpsrequesttest
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.properties.Delegates
+
+// Constants for the frequencies
+const val low_frequency: Long = 3000 // 3 seconds
+const val medium_frequency: Long = 2000 // 2 seconds
+const val high_frequency: Long = 1000 // 1 second
+
+class MainActivity : AppCompatActivity() {
+
+    // Runnable variable for repeating the action
+    private var runnable: Runnable by Delegates.notNull()
+
+    // Handler for infinite loop
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Handler for infinite loop
+        handler = Handler()
+
+        // Runnable which sends https request
+        runnable = Runnable {
+            // Send https request
+            sendGet()
+
+            handler.postDelayed(runnable, low_frequency)
+        }
+
+        // Start infinite loop
+        handler.post(runnable)
+    }
+
+    private fun sendGet() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val url = URL("https://www.google.com/")
+
+            with(url.openConnection() as HttpURLConnection) {
+                requestMethod = "GET"
+            }
+        }
+    }
+
+    override fun onPause() {
+        // Stop the loop
+        handler.removeCallbacks(runnable)
+
+        super.onPause()
+    }
+}
