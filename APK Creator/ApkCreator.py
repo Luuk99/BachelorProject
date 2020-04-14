25e79361f1dc535877c2e3cc1ca46aa12a539bd5
luuk <luuk9926@gmail.com>
2020-04-14 20:50:14 +0200

    Raw Results - Accelerometer & Ambient Light

--- APK Creator/ApkCreator.py
+++ APK Creator/ApkCreator.py
@@ -3,100 +3,74 @@ import os
 import fileinput
 import sys
 
+# Constant for the interval app names that are
+app_names = ["e.www.cameratest", "e.www.cpufactorialtest", "e.www.gpstest", "e.www.httpsrequesttest", "e.www.writelocaltest", "e.www.writeroomtest" "e.www.writesdtest"]
+small_app_names = []
+
 # Constants for the lines to look for per app
-line_camera = ""
-line_cpufactorial = "handler.postDelayed(runnable,"
+line_standard = "handler.postDelayed(runnable,"
 line_gps = "startLocationUpdates("
 
 # Constants for the options for the app
 frequency_options = ["low_frequency", "medium_frequency", "high_frequency"]
 priority_options = ["low_priority", "medium_priority", "high_priority"]
 
-# Function which returns the search line for the given app
-def searchline_for_app(app):
-    if (app == "CameraTest"):
-        return line_camera
-    elif (app == "CpuFactorialTest"):
-        return line_cpufactorial
-    elif (app == "GpsTest"):
-        return line_gps
+# Returns the search line for the given app
+def searchline_for_app(app_name):
+   if (app_name == "e.www.gpstest"):
+      return line_gps
+   else:
+      return line_standard
 
-# Function which handles the selected variable
-def handle_variable_result(app, search_line, frequency):
-    if (app == "GpsTest"):
-        # Ask the user to also select the priority variable
-        priority_question = [
-            {
-                'type': 'list',
-                'name': 'priority',
-                'message': 'Which priority do you want to use?',
-                'choices': priority_options
-            }
-        ]
-        answers = PyInquirer.prompt(priority_question)
-        priority = answers['priority']
+# Function that creates the apk for a given frequency
+def build_apk(app_name, frequency):
+   # Get the search line
+   search_line = searchline_for_app(app_name)
+   # Check if app uses standard frequencies
+   if (app_name == "e.www.gpstest"):
+      # Open the MainActivity.kt file
+      for line in fileinput.input(os.getcwd() + '/../Apps/' + app_name + '_low_frequency/app/src/main/java/e/www/' + app.lower() + '/MainActivity.kt', inplace=True):
+         if (line.strip() == search_line):
+            print("startLocationUpdates(" + frequency + ", " + priority + ")")
+         else:
+            sys.stdout.write(line)
+   else:
+      # Open the MainActivity.kt file
+      for line in fileinput.input(os.getcwd() + '/../Apps/' + app_name + '_low_frequency/app/src/main/java/e/www/' + app.lower() + '/MainActivity.kt', inplace=True):
+         if (line.strip() == search_line):
+            print("handler.postDelayed(runnable," + frequency + ")")
+         else:
+            sys.stdout.write(line)
+   # TODO: Build the APK
 
-        # Edit the file
-        for line in fileinput.input(os.getcwd() + '/../Apps/' + app + '/app/src/main/java/e/www/' + app.lower() + '/MainActivity.kt', inplace=True):
-            if (line.strip() == search_line):
-                print("startLocationUpdates(" + frequency + ", " + priority + ")")
-            else:
-                sys.stdout.write(line)
-        print("Variables updated successfully!")
-    else:
-        print(frequency)
+# Function that creates all apk's for the given app
+def build_app_versions(app_name):
+   # Decode the low_frequency app
+   os.system('java -jar /usr/local/bin/apktool.jar d /../Apps/{0}_low_frequency.apk'.format(app_name))
+   # Create the medium_frequency version
+   build_apk(app_name, "medium_frequency")
+   # Create the high_frequency version
+   build_apk(app_name, "high_frequency")
+   
+# Function which handles confirmation of the building
+def handle_confirm_result(confirm):
+   if confirm:
+      # Start building the apps
+      for app_name in app_names:
+         build_app_versions(app_name)
+      # Alert user of completion
+      print("APK building completed")
+   else:
+      print("APK building aborted")
 
-# Function which checks whether the user wants to edit the variable or not
-def handle_edit_result(app, line, edit):
-    if edit:
-        # Ask user to select the new frequency
-        frequency_question = [
-            {
-                'type': 'list',
-                'name': 'frequency',
-                'message': 'Which frequency do you want to use?',
-                'choices': frequency_options
-            }
-        ]
-        answers = PyInquirer.prompt(frequency_question)
-        handle_variable_result(app, line, answers['frequency'])
-    # TODO: Allow user to create APK straight away
-
-# Asks user whether he wants to edit the current variable
-def ask_to_edit(app, line):
-    edit_question = [
-        {
-            'type': 'confirm',
-            'message': 'Do you want to change the line \"' + line + '\" ?',
-            'name': 'edit',
-            'default': True,
-        }
-    ]
-    answers = PyInquirer.prompt(edit_question)
-    handle_edit_result(app, line, answers['edit'])
-
-# Function which handles the selected app
-def handle_app_result(app):
-    # Open main activity of the app
-    file = open(os.getcwd() + '/../Apps/' + app + '/app/src/main/java/e/www/' + app.lower() + '/MainActivity.kt', "r")
-    with file as f:
-        search_line = searchline_for_app(app)
-        lines = f.readlines()
-        for line in lines:
-            if (line.strip().startswith(search_line)):
-                file.close()
-                ask_to_edit(app, line.strip())
-
-# Ask user to select the app they want to create the APK for
-app_questions = [
+# Ask user to confirm building apk's
+confirm_question = [
     {
-        'type': 'list',
-        'name': 'app',
-        'message': 'Which app do you want to build?',
-        'choices': [
-            'CameraTest', 'CpuFactorialTest', 'GpsTest'
-        ]
+        'type': 'confirm',
+        'name': 'build',
+        'message': 'Are you sure you want to build all apk\'s in this directory?',
+        'default': True,
     }
 ]
-answers = PyInquirer.prompt(app_questions)
-handle_app_result(answers['app'])
+answers = PyInquirer.prompt(confirm_question)
+handle_confirm_result(answers['build'])
